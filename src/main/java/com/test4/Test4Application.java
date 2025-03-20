7cbb495f9d1344470c8452549b92a774afbf2159
mdwaliur02 <147179572+mdwaliur02@users.noreply.github.com>
2025-03-21 00:30:32 +0530

    Update Test4Application.java

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -8,6 +8,11 @@ public class Test4Application {
 
 	public static void main(String[] args) {
                 int x = 100;
+		string name = mike;
+		boolean isPresent = false;
+		double y = 200.3;
+		char ch = 'a';
+		float f = 10.3;
 		SpringApplication.run(Test4Application.class, args);
 	}
 
