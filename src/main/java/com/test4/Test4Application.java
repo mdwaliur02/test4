bb46fe12dff6be2e6f70f8ecbaf8ce09194dddeb
mdwaliur02 <147179572+mdwaliur02@users.noreply.github.com>
2025-03-21 00:45:41 +0530

    Update Test4Application.java

--- src/main/java/com/test4/Test4Application.java
+++ src/main/java/com/test4/Test4Application.java
@@ -7,6 +7,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Test4Application {
 
 	public static void main(String[] args) {
+		int x = 100;
+		double y = 200;
+		float z = 10.3;
+		straing name = mike;
+		boolean isPresent = false;
 		SpringApplication.run(Test4Application.class, args);
 	}
 
